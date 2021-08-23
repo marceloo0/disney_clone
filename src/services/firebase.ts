b917008671dbf2fe69c6dc4d8df2fd997c42e5d5
Marcelo Barbosa <62026017+marceloppb@users.noreply.github.com>
2021-08-23 17:00:25 -0300

    atualizando variaveis de ambiente

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -5,13 +5,13 @@ import "firebase/firestore";
 
 if (!firebase.apps.length) {
   firebase.initializeApp({
-    apiKey: "AIzaSyC3-MGgKxTagpI1pqgRlbZ5x4hxfHHjP8o",
-    authDomain: "disneyclonplus.firebaseapp.com",
-    databaseURL: "https://disneyclonplus-default-rtdb.firebaseio.com",
-    projectId: "disneyclonplus",
-    storageBucket: "disneyclonplus.appspot.com",
-    messagingSenderId: "623225645179",
-    appId: "1:623225645179:web:624ed9e2a6dbda0d9d5ade"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
   })
 };
 
